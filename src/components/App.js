8b81255520b0eb5639fcef71d12120f9f49ce987
devmobin <mobinheydari114@gmail.com>
2020-06-27 04:03:49 +0430

    get access to the state in component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import SongList from './SongList';
 
 function App() {
   return (
     <div className="App">
-      App
+      <SongList />
     </div>
   );
 }
